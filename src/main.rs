62928e98ccf09cf8da3e1d208b1d5ce9bd0e5280
Kodai Kabasawa <kabaaa1126@gmail.com>
2022-04-21 20:38:55 +0900

    add termios configuration

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
-use std::{fs, os::unix::prelude::AsRawFd};
+use std::{
+    fs::{self, File},
+    io::{self, Write},
+    os::unix::prelude::AsRawFd,
+};
 
-use nix::unistd::isatty;
+use nix::{
+    errno::Errno,
+    sys::termios::{cfmakeraw, tcgetattr, tcsetattr, OutputFlags, SetArg, Termios},
+    unistd::isatty,
+};
+
+pub fn configuration(tty: &File) -> Result<Termios, io::Error> {
+    let is_tty = isatty(tty.as_raw_fd()).expect("not atty");
+    if !is_tty {
+        return Err(io::Error::from(Errno::ENOTTY));
+    }
+    let prev_conf = tcgetattr(tty.as_raw_fd()).unwrap();
+    let mut conf = prev_conf.clone();
+    cfmakeraw(&mut conf);
+    conf.output_flags |= OutputFlags::OPOST;
+    tcsetattr(tty.as_raw_fd(), SetArg::TCSANOW, &conf).unwrap();
+    return Ok(conf);
+}
 
 fn main() {
     let tty = fs::OpenOptions::new()
@@ -9,5 +30,5 @@ fn main() {
         .open("/dev/tty")
         .expect("cannot open tty");
     let is_tty = isatty(tty.as_raw_fd()).expect("not atty");
-    println!("{}", is_tty);
+    println!("is tty {}", is_tty);
 }
