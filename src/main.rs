237c79705574b5603d174283173bf4c4a165af4d
Kodai Kabasawa <kabaaa1126@gmail.com>
2022-04-21 19:52:30 +0900

    get tty

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::{fs, os::unix::prelude::AsRawFd};
+
+use nix::unistd::isatty;
+
 fn main() {
-    println!("Hello, world!");
+    let tty = fs::OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open("/dev/tty")
+        .expect("cannot open tty");
+    let is_tty = isatty(tty.as_raw_fd()).expect("not atty");
+    println!("{}", is_tty);
 }
